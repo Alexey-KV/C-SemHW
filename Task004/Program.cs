fc50c8bce2f665fe5eaff5c845177512d7ae590b
Alexey Kuznetchov <Kuznetchovav@gmail.com>
2023-03-02 23:25:22 +0300

    Added Seminar01 home work

---
+++ Task004/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Please enter the number (A): ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter the number (B): ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter the number (C): ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int maxNumber = numberA;
+
+if(maxNumber < numberC)
+{
+    maxNumber = numberC;
+    Console.Write($"Maximum number is C ({numberC})");
+}
+else if(maxNumber < numberB)
+{
+    Console.Write($"Maximum number is B {numberB}");
+}
+else
+{
+    Console.Write($"Maximum number is A {numberA}");
+}
+
+
+
---
+++ Task006/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Please enter the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int temp = number%2;
+
+if(temp == 0)
+{
+    Console.Write("Number you entered is even.");
+}
+else
+{
+    Console.Write("Number you entered is not even.");
+}
