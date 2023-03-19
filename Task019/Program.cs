a7afabddcdbb9021901e96e9e86a967e193ab577
Alexey Kuznetchov <Kuznetchovav@gmail.com>
2023-03-19 23:15:59 +0300

    Added Sem HW up to Task023

---
+++ Task019/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет, 12821 -> да, 23432 -> да.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+string Palindrome(int a, int b, int c, int d, int e)
+{
+    string result = "";
+    if(a == e && b == d)
+    {
+        result = "Число является палиндромом";
+    }
+    else
+    {
+        result = "Число не является палиндромом";
+    }
+    return result;
+}
+
+int A = Prompt("Введите 1 цифру пятизначного числа: ");
+int B = Prompt("Введите 2 цифру пятизначного числа: ");
+int C = Prompt("Введите 3 цифру пятизначного числа: ");
+int D = Prompt("Введите 4 цифру пятизначного числа: ");
+int E = Prompt("Введите 5 цифру пятизначного числа: ");
+
+string palindrome = Palindrome(A, B, C, D, E);
+Console.WriteLine(palindrome);
+
+// sorry не было временни придумать нормальный ввод (были бы знания, времени было бы не нужно)) 
---
+++ Task023/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Please enter the number (N): ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for( int i = 1; i <= number; i++)//Ex. Цикл через 1: i+=2  <->  i = i + 2
+{
+   Console.Write($"{Math.Pow(i, 3)} ");
+}
